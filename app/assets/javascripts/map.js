33b23cd02e45e9f599492fe67c859e705955ce84
gilbertoTN <gtovar@incaztech.com>
2013-04-08 01:50:12 -0500

    Facebook link y google maps con mark y shadow

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,41 +1,47 @@
 (function() {
+
+  // Defining variables that need to be available to some functions
+  var map, infoWindow;
+
   window.onload = function() {
     // Creating an object literal containing the properties
     // we want to pass to the map
     var options = {
-      zoom: 12,
-      center: new google.maps.LatLng(40.7257, -74.0047),
+      zoom: 15,
+      center: new google.maps.LatLng(25.738647, -100.355483),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     // Creating the map
-    var map = new google.maps.Map(document.getElementById('map'), options);
-
-
-var image = new google.maps.MarkerImage(
-  'assets/logo.png',
-  new google.maps.Size(32, 37), // The Size
-  new google.maps.Point(65, 0), // The origin
-  new google.maps.Point(16, 35) // The anchor
-  )
-
+    map = new google.maps.Map(document.getElementById('map'), options);
 
     // Adding a marker to the map
+    var mamado = new google.maps.MarkerImage('assets/markergym.png');
+    var shadow = new google.maps.MarkerImage('assets/shadow.png',null,null,new google.maps.Point(25, 65) );
+
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(40.7257, -74.0047),
+      position: new google.maps.LatLng(25.738647, -100.355483),
       map: map,
-      title: 'Click me',
-      icon: image    });
-
-
-    var infowindow = new google.maps.InfoWindow({
-      content:'<div class="condon"><h1 class="titulo1">Hello world<h2></div>'
+      title: 'Planet GYM (haz click aqui)',
+      icon: mamado,
+      shadow: shadow
     });
 
-    // Adding a click event to the marker
-    google.maps.event.addListener(marker, 'click', function() {
-    // Calling the open method of the infoWindow
-    infowindow.open(map, marker);
-    });
+      google.maps.event.addListener(marker, 'click', function() {
+          infoWindow = new google.maps.InfoWindow({
+            content: '<div id="info">' +
+            '<img src="assets/logo.png" alt="" />' +
+            '<h2>PLANET GYM</h2>' +
+            '<p>Direccion:</p>' +
+            '<p>Uxmal, Col. Unidad Modelo</p>' +
+            '<p>Monterrey, Nuevo Leon</p>' +
+          '<p><a href="http://www.planetgym.com">Sitio Oficial</a></p>' +
+            '</div>',
+            position: new google.maps.LatLng(25.738647, -100.355483)
+            });
+          infoWindow.open(map);
+
+      });
+
 
   };
 })();
